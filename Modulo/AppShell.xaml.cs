dfdf2302b4ec873da699169da26667f6be3a47d8
Everett Lopez <everettlopezsr@gmail.com>
2025-10-03 19:34:11 -0600

    Created project, implemented basic homepage and calendar page.

---
+++ Modulo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Modulo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Modulo/CalendarPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace Modulo;
+
+public partial class CalendarPage : ContentPage
+{
+    public CalendarPage()
+    {
+        InitializeComponent();
+    }
+}
